a163aba04f3ebcd0a7182777d9a429bdb80ddf86
Paul Aghion <a.paul94@yahoo.com>
2015-06-10 20:59:23 +0300

    added iOS project

---
+++ iOS/UatsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UatsApp
+//
+//  Created by Student on 10/06/15.
+//  Copyright (c) 2015 Paul Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
