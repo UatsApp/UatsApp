26bb41c1d730c9b9c3bcd39ecfec3e47c063ad47
Paul Aghion <a.paul94@yahoo.com>
2015-06-11 13:40:00 +0300

    -added storyboard -added LoginVC -added SignupVC -added UatsAppVC -implemented SignupVC

---
+++ iOS/UatsApp/SignupVC.swift
@@ -0,0 +1,136 @@
+//
+//  SignupVC.swift
+//  UatsApp
+//
+//  Created by Student on 10/06/15.
+//  Copyright (c) 2015 Paul Paul. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfrimPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func signupTapped(sender: AnyObject) {
+        var username:NSString = txtUsername.text as NSString
+        var password:NSString = txtPassword.text as NSString
+        var comfirm_password:NSString = txtConfrimPassword.text as NSString
+        
+        if(username.isEqualToString("") || password.isEqualToString(""))
+        {
+            var alertView:UIAlertView = UIAlertView()
+            alertView.title = "Sign up Failed!"
+            alertView.message = "Please enter username and password"
+            alertView.delegate = self
+            alertView.addButtonWithTitle("OK")
+            alertView.show()
+        }else if (!password.isEqual(comfirm_password)){
+            var alertView:UIAlertView = UIAlertView()
+            alertView.title = "Sign up Failed!"
+            alertView.message = "Passwords don't match!"
+            alertView.delegate = self
+            alertView.addButtonWithTitle("OK")
+            alertView.show()
+        } else {
+            var post:NSString = "username=\(username)&password=\(password)&c_password=\(comfirm_password)"
+            NSLog("Post data: %@",post);
+            
+            var url:NSURL = NSURL(string: "https://pulamea.com")!
+            var postData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
+            var postLength:NSString = String(postData.length)
+            
+            var request:NSMutableURLRequest = NSMutableURLRequest(URL:url)
+            request.HTTPMethod = ("POST")
+            request.HTTPBody = postData
+            request.setValue(postLength as String, forHTTPHeaderField:"Content-Length")
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField:"Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            
+            var responseError: NSError?
+            var response:NSURLResponse?
+            var urlData:NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &responseError)
+            if(urlData != nil){
+                let res = response as! NSHTTPURLResponse!;
+                NSLog("Response code: %ld", res.statusCode)
+                if(res.statusCode >= 200 && res.statusCode < 300){
+                    var responseData:NSString = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+                    NSLog("Response ==> %@", responseData)
+                    var error:NSError?
+                    let jsonData:NSDictionary = NSJSONSerialization.JSONObjectWithData(urlData!, options: NSJSONReadingOptions.MutableContainers, error: &error) as! NSDictionary
+                    let success:NSInteger = jsonData.valueForKey("success") as! NSInteger
+                    
+                    NSLog("Success %@", success);
+                    
+                    if(success == 1){
+                        NSLog("Sign Up Success");
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    }else{
+                        var error_msg:NSString
+                        if jsonData["error_message"] as? NSString != nil
+                        {
+                            error_msg = jsonData["error_message"] as! NSString
+                        }else{
+                            error_msg = "Unknown Error"
+                        }
+                        var alertView:UIAlertView = UIAlertView()
+                        alertView.title = "Sign Up Failed!"
+                        alertView.message = error_msg as String
+                        alertView.delegate = self
+                        alertView.addButtonWithTitle("OK")
+                        alertView.show()
+                    }
+                }else{
+                    var alertView:UIAlertView = UIAlertView()
+                    alertView.title = "Sign Up Faile!"
+                    alertView.message = "Connection Failed"
+                    alertView.delegate = self
+                    alertView.addButtonWithTitle("OK")
+                    alertView.show()
+                }
+            }else{
+                var alertView:UIAlertView = UIAlertView()
+                alertView.title = "Sign in Failed!"
+                alertView.message = "Connection Failure"
+                if let error = responseError{
+                    alertView.message = (error.localizedDescription)
+                }
+                alertView.delegate = self
+                alertView.addButtonWithTitle("OK")
+                alertView.show()
+            }
+        }
+    }
+
+    
+
+    @IBAction func gotoLogin(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/UatsApp/UatsAppVC.swift
@@ -0,0 +1,49 @@
+//
+//  UatsAppVC.swift
+//  UatsApp
+//
+//  Created by Student on 10/06/15.
+//  Copyright (c) 2015 Paul Paul. All rights reserved.
+//
+
+import UIKit
+
+class UatsAppVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var txtusername: UILabel!
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedin:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        
+        if(isLoggedin != 1){
+            self.performSegueWithIdentifier("goto_login", sender: self)
+        }
+        else{
+            self.txtusername.text = prefs.valueForKey("USERNAME") as? String
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
