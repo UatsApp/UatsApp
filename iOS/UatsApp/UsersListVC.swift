d88f3980e6eb5d01ae4f6837aa5f12dd18a25115
Paul Aghion <a.paul94@yahoo.com>
2015-07-15 19:49:36 +0300

    Implemented UITableView Implemented TEST segue

---
+++ iOS/UatsApp/UsersListVC.swift
@@ -0,0 +1,67 @@
+//
+//  UsersListVC.swift
+//  UatsApp
+//
+//  Created by Student on 15/07/15.
+//  Copyright (c) 2015 Paul Paul. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class UsersListVC: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let cellIdentifier = "cell_text"
+    let someUsers = ["Paul", "Cata", "Boici"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return someUsers.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        let row = indexPath.row
+        cell.textLabel?.text = someUsers[row]
+        return cell
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        let row = indexPath.row
+        println(someUsers[row])
+        performSegueWithIdentifier("call", sender: someUsers[row])
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "call" {
+            let chatController = segue.destinationViewController as! ChatViewController
+            chatController.user = sender as! String
+            
+        }
+    }
+
+
+}
