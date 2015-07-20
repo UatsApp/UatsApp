8f6c54a464e561f18fab372ddcb0f68eb8c4713a
Paul Aghion <a.paul94@yahoo.com>
2015-07-21 01:30:50 +0300

    Implemented history call to Backend

--- iOS/UatsApp/ChatViewController.swift
+++ iOS/UatsApp/ChatViewController.swift
@@ -7,37 +7,105 @@
 //
 
 import UIKit
+import Alamofire
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet var historyTable: UITableView!
+    let cellHistory = "cell_message"
     var user: String!
+    var history:[History] = []
+    var user2:NSArray = []
+    
+    
+    //   var muiecupula = user2[0] as! String
     
     @IBOutlet weak var userLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.checkRelation()
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        userLabel.text = user
+        self.navigationItem.title = user2[1] as? String
+        historyTable.delegate = self
+        historyTable.dataSource = self
+        println(user2[1])
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    func checkRelation(){
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        var userUsername = prefs.valueForKey("USERNAME") as! String
+        Alamofire.request(.POST, "http://uatsapp.tk/UatsAppWebDEV/history.php", parameters: ["identifier": "\(user2[0])" , "loggedUsername":"\(userUsername)"], encoding: .JSON)
+            .responseJSON { _, _, JSON, _ in
+                if let jsonResult = JSON?.valueForKey("history") as? Array<Dictionary<String,String>>{
+                    var i = 0
+                    for (i = 0; i < jsonResult.count; i++)
+                    {
+                        
+                        let relID = jsonResult[i]["id_c"]
+                        let from = jsonResult[i]["_from"]
+                        let to = jsonResult[i]["_to"]
+                        let message = jsonResult[i]["message"]
+                        let time = jsonResult[i]["_time"]
+                        
+                        var currentHistory = History()
+                        currentHistory.id_c = relID!.toInt()!
+                        currentHistory._from = from!.toInt()!
+                        currentHistory._to = to!.toInt()!
+                        currentHistory.message = message!
+                        currentHistory._time = time!
+                        println("asda\(message)")
+                        self.history.append(currentHistory)
+                        //  completion()
+                        
+                    }
+                    //
+                    //  completion()
+                }
+                println(JSON)
+                self.historyTable.reloadData()
+        }
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        var numbOfRows = history.map({$0.message})
+        println("Sugeo Ramona+\(numbOfRows.count)")
+        return history.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellHistory, forIndexPath: indexPath) as! UITableViewCell
+        let row = indexPath.row
+        let messages = history.map({$0.message})
+        println(messages[row])
+        cell.textLabel?.text = messages[row]
+        return cell
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
--- iOS/UatsApp/UsersListVC.swift
+++ iOS/UatsApp/UsersListVC.swift
@@ -12,22 +12,19 @@ import Alamofire
 class UsersListVC: UIViewController,UITableViewDataSource, UITableViewDelegate {
     
     var usersList:[Users] = []
-    var history:[History] = []
+    //    var history:[History] = []
     
     
     @IBOutlet var tableView: UITableView!
-  
+    
     let cellIdentifier = "cell_text"
-    //let someUsers = ["Paul", "Cata", "Boici"]
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         self.getUsers()
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -58,39 +55,6 @@ class UsersListVC: UIViewController,UITableViewDataSource, UITableViewDelegate {
         }
     }
     
-    func checkRelation(id:Int, completion: (() -> Void)!){
-        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
-        var userUsername = prefs.valueForKey("USERNAME") as! String
-        Alamofire.request(.POST, "http://uatsapp.tk/UatsAppWebDEV/history.php", parameters: ["identifier": "\(id)" , "loggedUsername":"\(userUsername)"], encoding: .JSON).responseJSON { _, _, JSON, _ in
-            if let jsonResult = JSON as? Array<Dictionary<String,String>>{
-                var i = 0
-                for (i = 0; i < jsonResult.count; i++)
-                {
-        
-                    let relID = jsonResult[i]["id_c"]
-                    let from = jsonResult[i]["_from"]
-                    let to = jsonResult[i]["_to"]
-                    let message = jsonResult[i]["message"]
-                    let time = jsonResult[i]["_time"]
-                    
-                    var currentHistory = History()
-                    currentHistory.id_c = relID!.toInt()!
-                    currentHistory._from = from!.toInt()!
-                    currentHistory._to = to!.toInt()!
-                    currentHistory.message = message!
-                    currentHistory._time = time!
-                    self.history.append(currentHistory)
-                  //  completion()
-                }
-                
-                //self.tableView.reloadData()
-              //  completion()
-            }
-            println(JSON)
-            completion()
-        }
-    
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -104,7 +68,12 @@ class UsersListVC: UIViewController,UITableViewDataSource, UITableViewDelegate {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usersList.count
     }
+    
+    
     /////////////////////AFISEZ USERNAMEU-RILE////////////////////////////////
+    
+    
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
         let row = indexPath.row
@@ -112,36 +81,36 @@ class UsersListVC: UIViewController,UITableViewDataSource, UITableViewDelegate {
         cell.textLabel?.text = urr[row]
         return cell
     }
+    
+    
     ////////////////////VAD CINE S-O SELECTAT SI CE ID ARE; FAC PEPARE SEGUE DE USERNAME DEOCAMDATA!!!!! URMEAZA SA FAC DE HISTORY//////////////////
+    
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         let row = indexPath.row
         let urr = usersList.map({$0.user_id})
         let urr_user = usersList.map({$0.username})
+        
+        var idPlusUN = [urr[row], urr_user[row]]
+        
         println("lol+\(urr[row])")
-        
-        self.checkRelation(urr[row]) { () -> Void in
-            self.performSegueWithIdentifier("call", sender: urr_user[row])
-
-        }
-        
-        
+        self.performSegueWithIdentifier("call", sender: idPlusUN)
         
     }
     
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         if segue.identifier == "call" {
             let chatController = segue.destinationViewController as! ChatViewController
-            chatController.user = sender as! String
-            
+            chatController.user2 = sender as! NSArray
         }
     }
-
-
+    
+    
 }
