ca9a43846b03426b917c3da27b763f74bb545564
Paul Aghion <a.paul94@yahoo.com>
2015-08-03 11:00:30 +0300

    Fixed Ciprian's Issue

--- iOS/UatsApp/ChatViewController.swift
+++ iOS/UatsApp/ChatViewController.swift
@@ -199,12 +199,13 @@ class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         bottomLayoutConstraint.constant = CGRectGetMaxY(view.bounds) - CGRectGetMinY(convertedKeyboardEndFrame)
         bottomLayoutConstraintSendButton.constant = CGRectGetMaxY(view.bounds) - CGRectGetMinY(convertedKeyboardEndFrame)
+        if self.cellMetas.count > 0{
         let delay = 0.1 * Double(NSEC_PER_SEC)
         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
         
         dispatch_after(time, dispatch_get_main_queue(), {
             self.historyTable.scrollToRowAtIndexPath(NSIndexPath(forRow: self.cellMetas.count-1 , inSection: 0), atScrollPosition: .Bottom, animated: false)
-        })
+        })}
         UIView.animateWithDuration(animationDuration, delay: 0.0, options: .BeginFromCurrentState | animationCurve, animations: {
             self.view.layoutIfNeeded()
             }, completion: nil)
