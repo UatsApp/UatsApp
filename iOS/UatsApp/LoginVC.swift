19838a85e03e2bbe1e6ecd6933f9bf93153a5927
Paul Aghion <a.paul94@yahoo.com>
2015-09-15 16:13:09 +0300

    Both iOS 9 and 8 compatible Needs FBSDK to be integrated.

--- iOS/UatsApp/LoginVC.swift
+++ iOS/UatsApp/LoginVC.swift
@@ -290,7 +290,7 @@ class LoginVC: UIViewController, FBSDKLoginButtonDelegate {
                         let user_id:Int = jsonData.valueForKey("user_id") as! Int
                         let SessionToken:String = jsonData.valueForKey("token") as! String
                         
-                        try! KeyChain.saveData(["token" : "\(SessionToken)","user_id":"\(user_id)"], forUserAccount: "\(username)")
+                 //       try! KeyChain.saveData(["token" : "\(SessionToken)","user_id":"\(user_id)"], forUserAccount: "\(username)")
                         
                         NSLog("Login SUCCESS");
                         
