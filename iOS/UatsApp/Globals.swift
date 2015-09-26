5be9ae80a6b610f813f6427721fdf2a650a586a4
Paul Aghion <a.paul94@yahoo.com>
2015-09-26 15:29:13 +0300

    Fixed issue where if KeyChain was null app crashes

--- iOS/UatsApp/Globals.swift
+++ iOS/UatsApp/Globals.swift
@@ -9,43 +9,45 @@
 import Foundation
 
 var myUserName: String{
-    get {
-        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+get {
+    let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
     
-        if let myUserName  = prefs.valueForKey("USERNAME") as? String{
-            return myUserName
-        }else{
-            return "Nu exista Username in Preferitne"
-        }
+    if let myUserName  = prefs.valueForKey("USERNAME") as? String{
+        return myUserName
+    }else{
+        return "Nu exista Username in Preferitne"
     }
+}
 }
 
 var userAvatar: String{
-    get{
-        let KeyChainData = KeyChain.loadDataForUserAccount("profileIMG")
-        
-        if let imagePath = KeyChainData!["userImage"]{
-            //print(imagePath)
-            return imagePath as! String
-        }else{
-            return ""
-        }
+get{
+    let KeyChainData = KeyChain.loadDataForUserAccount("profileIMG")
+    
+    if KeyChainData != nil{
+        let imagePath = KeyChainData!["userImage"]
+        //print(imagePath)
+        return imagePath as! String
+    }else{
+        let defaultImageURL = NSBundle.mainBundle().URLForResource("1024x1024", withExtension: "png")
+        return defaultImageURL!.path!
     }
+}
 }
 
 
 var token: String {
-    get {
-        var KeyChainData = KeyChain.loadDataForUserAccount("\(myUserName)")
-        
-        if let tokenString = KeyChainData!["token"] {
-            NSLog("Tokenul sessiunii este: \(tokenString)")
-            return tokenString as! String
-        } else {
-            NSLog("nu exista token")
-            return "OMG"
-        }
+get {
+    var KeyChainData = KeyChain.loadDataForUserAccount("\(myUserName)")
+    
+    if let tokenString = KeyChainData!["token"] {
+        NSLog("Tokenul sessiunii este: \(tokenString)")
+        return tokenString as! String
+    } else {
+        NSLog("nu exista token")
+        return "OMG"
     }
+}
 }
 
 var userID: Int {
@@ -60,14 +62,14 @@ get {
         NSLog("Nu exista USERID IN KeyChain")
         return 0
     }
-  }
+}
 }
 
 var enrollStep: Int{
 get{
     let KeyChainData = KeyChain.loadDataForUserAccount("enroll")
-    
-    if let enrollmentStep = KeyChainData!["enroll"]{
+    if KeyChainData != nil{
+        let enrollmentStep = KeyChainData!["enroll"]
         let enroll:Int? = NSNumberFormatter().numberFromString(enrollmentStep as! String)?.integerValue
         return enroll!
     }else{
