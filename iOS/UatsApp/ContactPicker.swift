61aed4b93bbd7899344b0ad88a53fcec3d0ccd91
Paul Aghion <a.paul94@yahoo.com>
2015-10-01 19:25:22 +0300

    Updated 'Crashlytics'

--- iOS/UatsApp/ContactPicker.swift
+++ iOS/UatsApp/ContactPicker.swift
@@ -14,7 +14,6 @@ class ContactPicker: UIViewController {
     @IBOutlet weak var continueButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
